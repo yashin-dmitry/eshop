a10da755b47bf7f73c7e18ef273f20f5be40fd53
Dmitry <exigo53@gmail.com>
2024-03-20 23:35:51 +0300

    homework 13.2

--- src/products.py
+++ src/products.py
@@ -14,13 +14,16 @@ class Product:
         price (float): Цена продукта.
         quantity (int): Количество продукта в наличии.
     """
-    def __init__(self, name: str, description: str, price: float, quantity: int):
+
+    def __init__(self, name: str, description: str, price: float,
+                 quantity: int):
         if not isinstance(name, str):
             raise TypeError("имя должно быть строкой")
         if not isinstance(description, str):
             raise TypeError("описание должно быть строкой")
         if not isinstance(price, (int, float)):
-            raise TypeError("цена должна быть числом с плавающей точкой или целым числом")
+            raise TypeError("цена должна быть числом с плавающей точкой"
+                            " или целым числом")
         if not isinstance(quantity, int):
             raise TypeError("количество должно быть целым числом")
         if price < 0:
@@ -30,6 +33,28 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
 
+
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("цена должна быть числом с плавающей точкой"
+                            " или целым числом")
+        if value < 0:
+            raise ValueError("цена не может быть отрицательной")
+        self._price = value
+
+    @price.deleter
+    def price(self):
+        del self._price
+
+    @classmethod
+    def new_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
